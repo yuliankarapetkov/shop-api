428148220a04f2b7fe908d3e54c34908c5a49b43
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2018-06-17 15:16:26 +0300

    added nodemon, morgan and basic error handling

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+   const error = new Error('Not found');
+   error.status = 404;
+   next(error);
+});
+
+app.use((err, req, res, next) => {
+   res.status(err.status || 500).json({
+      error: {
+          message: err.message
+      }
+   });
+});
 
 module.exports = app;
