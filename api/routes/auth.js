a89af731f7f6c42055953915972b18ebeb2ad58f
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2018-06-19 17:28:50 +0300

    login added

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -33,6 +34,53 @@ router.post('/signup', (req, res, next) => {
                 });
         }
     });
+});
+
+router.post('/login', (req, res, next) => {
+   User.find({ email: req.body.email })
+       .exec()
+       .then(user => {
+           if (user.length < 1) {
+               return res.status(401).json({
+                   message: 'Auth failed'
+               });
+           }
+
+           bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+
+                if (result) {
+                    const token = jwt.sign(
+                        {
+                            _id: user[0]._id,
+                            email: user[0].email
+                        },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: '1h'
+                        }
+                    );
+
+                    res.status(200).json({
+                        message: 'Auth successful',
+                        token: token
+                    });
+                } else {
+                    res.status(401).json({
+                        message: 'Auth failed'
+                    });
+                }
+           });
+       })
+       .catch(err => {
+           res.status(500).json({
+               error: err
+           });
+       });
 });
 
 module.exports = router;
